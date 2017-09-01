e3510f7cf376d509f7f6fd2b1cd42d45bea53325
Kevin Dibble <kevindibble@iMac.local>
2017-09-01 16:36:54 +1200

    Added functionality to upload images to pages created

--- lib.php
+++ lib.php
@@ -30,6 +30,20 @@ function local_pages_extends_navigation(global_navigation $nav) {
     return local_pages_extend_navigation($nav);
 }
 
+function local_pages_pluginfile($course, $birecordorcm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+    $fs = get_file_storage();
+
+    $filename = array_pop($args);
+    $filepath = $args ? '/' . implode('/', $args) . '/' : '/';
+
+    if (!$file = $fs->get_file($context->id, 'local_pages', 'pagecontent', 0, $filepath, $filename) or $file->is_directory()) {
+        send_file_not_found();
+    }
+
+    \core\session\manager::write_close();
+    send_stored_file($file, null, 0, $forcedownload, $options);
+}
+
 function local_pages_build_menu(navigation_node $nav, $parent) {
     global $CFG, $DB;
     $records = $DB->get_records_sql("SELECT * FROM {local_pages} WHERE deleted=0 AND onmenu=1 " .
