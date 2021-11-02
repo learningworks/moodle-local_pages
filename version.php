61ca96df6e480509208561ff7598df7c18c67378
Jay <justinoncyber@gmail.com>
2021-11-02 14:52:00 +1300

    Update version.php

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->requires   = 2021051700;                       // This plugin requires Moodle VER 3.11.
 $plugin->version    = 2021102200;                       // This plugins version number.
-$plugin->release    = 'v1.9.3';                           // This plugins release number.
+$plugin->release    = 'v1.9';                           // This plugins release number.
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component  = 'local_pages';
